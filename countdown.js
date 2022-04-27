998197f7e060bfd85dfaabb608fff7ac177036c4
emanuel <ecastillo76@misena.edu.co>
2022-04-27 11:08:31 -0500

    document js for automatic discount on htmlpage

--- countdown.js
+++ countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Oct 12, 2021 16:30:00").getTime();
+var countDownDate = new Date("May 1, 2022 23:59:59").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
---
+++ js/promo-thursday.js
@@ -0,0 +1,37 @@
+// Here you put how much for discount
+const discount_percentage = 40
+
+const zero_english_year_price = 300
+const zero_english_month_price = 29
+const zero_english_lifetime_price = 547
+
+const ipa_course_price = 100
+const grammar_course_price = 60
+
+
+// Curso de Inglés desde 0
+let ze_today_price = zero_english_year_price - (zero_english_year_price * (discount_percentage/100))
+let ze_you_save = zero_english_year_price - ze_today_price
+
+document.getElementById("ze-today-price").innerHTML = '$' + ze_today_price + ' USD'
+document.getElementById("ze-you-save").innerHTML = '$' + ze_you_save + ' USD';
+document.getElementById("ze-discount-percentage").innerHTML = ' ('+ discount_percentage + '% de descuento)'
+
+
+// Curso de Fonética
+let ipa_today_price = ipa_course_price - (ipa_course_price * (discount_percentage/100))
+let ipa_you_save = ipa_course_price - ipa_today_price
+
+document.getElementById("ipa-today-price").innerHTML = '$' + ipa_today_price + ' USD'
+document.getElementById("ipa-you-save").innerHTML = '$' + ipa_you_save + ' USD';
+document.getElementById("ipa-discount-percentage").innerHTML = ' ('+ discount_percentage + '% de descuento)'
+
+
+
+// Grammar Course
+let gc_today_price = grammar_course_price - (grammar_course_price * (discount_percentage/100))
+let gc_you_save = grammar_course_price - gc_today_price
+
+document.getElementById("gc-today-price").innerHTML = '$' + gc_today_price + ' USD'
+document.getElementById("gc-you-save").innerHTML = '$' + gc_you_save + ' USD';
+document.getElementById("gc-discount-percentage").innerHTML = ' ('+ discount_percentage + '% de descuento)'
