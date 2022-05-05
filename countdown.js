73479e02f37c868a0a6034d5180cf122e4665363
emanuel <ecastillo76@misena.edu.co>
2022-05-05 17:23:43 -0500

    Thursday promo - may 5 2022

--- countdown.js
+++ countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("May 1, 2022 23:59:59").getTime();
+var countDownDate = new Date("May 6, 2022 21:59:59").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
