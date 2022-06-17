2ebf70fe93758782608e76db46b151507a0411a5
emanuel <ecastillo76@misena.edu.co>
2022-06-17 02:16:40 -0500

    dia sin iva colombia

--- countdown.js
+++ countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jun 10, 2022 19:00:00").getTime();
+var countDownDate = new Date("Jun 17, 2022 23:59:59").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
